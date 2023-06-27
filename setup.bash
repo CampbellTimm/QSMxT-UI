734efc7894d3cc502f6e55adacafc789aadd069d
Ashley Stewart <a.stewart.au@gmail.com>
2023-06-27 14:28:26 +1000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+echo "[DEBUG] In setup.bash"
+
+echo "[DEBUG] FRONTEND SETUP"
 cd frontend
+
+echo "[DEBUG] Running 'npm install' in `pwd`:"
 npm install
+
+echo "[DEBUG] Running 'npm build' in `pwd`:"
 npm build
-cd ..
 
-cd api
+echo "[DEBUG] API SETUP"
+cd ../api
+
+echo "[DEBUG] Updating package lists via apt-get update..."
 sudo apt-get update
-sudo apt-get install libsqlite3-dev
-npm install sqlite3 --build-from-source
-npm install
-npm i -g ts-node
 
+echo "[DEBUG] Installing libsqlite3-dev"
+sudo apt-get install libsqlite3-dev
+
+echo "[DEBUG] Installing sqlite3 via npm 'npm install sqlite3 --build-from-source'"
+npm install sqlite3 --build-from-source
+
+echo "[DEBUG] Installing API via 'npm install'"
+npm install
+
+echo "[DEBUG] Final step via 'npm i -g ts-node'"
+npm i -g ts-node
 
